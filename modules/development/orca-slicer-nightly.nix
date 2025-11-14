9aa372ee7167bcbc37c4b64378758d164f6fa9e1
Version33 <vee@versionthirtythr.ee>
2025-11-14 17:01:49 -0500

    update

--- modules/development/orca-slicer-nightly.nix
+++ modules/development/orca-slicer-nightly.nix
@@ -20,10 +20,10 @@
     (final: prev:
       let
         pname = "orca-slicer-nightly";
-        version = "nightly-2025-11-11";
+        version = "nightly-2025-11-14";
         src = pkgs.fetchurl {
           url = "https://github.com/SoftFever/OrcaSlicer/releases/download/nightly-builds/OrcaSlicer_Linux_AppImage_Ubuntu2404_nightly.AppImage";
-          hash = "sha256-CRZNKQqB7854vfwdmwA+QHEwhOic0FR+BfnFnKoMQHY=";
+          hash = "sha256-+J7iT1YRblblPShLhn4kN2rdYQMCwFebEuQ79ro80Fo=";
         };
         appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
       in
