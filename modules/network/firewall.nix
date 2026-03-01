2b77bd56b1bd136fa5a0b9ce5cfd9e8b047ac770
Version33 <vee@versionthirtythr.ee>
2026-02-28 22:41:35 -0500

    add a port to comment

--- modules/network/firewall.nix
+++ modules/network/firewall.nix
@@ -3,7 +3,7 @@
   flake.modules.nixos.firewall = _: {
     # Open ports in the firewall.
     networking.firewall.enable = true;
-    # networking.firewall.allowedTCPPorts = [ 3000 ];
+    # networking.firewall.allowedTCPPorts = [ 22 3210 ];
     # networking.firewall.allowedUDPPorts = [ 3000 ];
     # Or disable the firewall altogether.
     # networking.firewall.enable = false;
