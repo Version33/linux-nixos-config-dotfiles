aee770da05b10e6d3de553c419d929d67b8f5cb2
Version33 <vee@versionthirtythr.ee>
2026-03-19 16:53:48 -0400

    swap kernel to xanmod, also USB3-7 is fucked rn so I shortened the timeout

--- modules/boot/linux-kernel.nix
+++ modules/boot/linux-kernel.nix
@@ -6,12 +6,14 @@
       # Linux Kernel
       boot = {
         kernel.sysctl."kernel.sysrq" = 1; # Enable all SysRq functions
-        kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+        kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
         kernelParams = [
           "quiet"
           "splash"
           "loglevel=5"
           # "usbcore.autosuspend=-1"
+          # Shorten USB enumeration timeout to reduce boot delay from broken internal port (usb3-7)
+          "usbcore.initial_descriptor_timeout=5"
         ];
       };
 
