63612225c9e4606e5b6a8d90187c65020f1109b6
Version33 <vee@versionthirtythr.ee>
2026-03-30 08:48:43 -0400

    noctalia settings

--- modules/desktop/noctalia.nix
+++ modules/desktop/noctalia.nix
@@ -1,10 +1,13 @@
-{ self, inputs, ... }: {
-  perSystem = { pkgs, ... }: {
-    packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      inherit pkgs;
-      # settings =
-      #   (builtins.fromJSON
-      #     (builtins.readFile ./noctalia.json)).settings;
+{ self, inputs, ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+        inherit pkgs;
+        settings = # nix run nixpkgs#noctalia-shell ipc call state all > ./modules/desktop/noctalia.json
+          (builtins.fromJSON
+            (builtins.readFile ./noctalia.json)).settings;
+      };
     };
-  };
 }
